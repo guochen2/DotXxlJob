bfd54144870983ec6ec572adeee81c2d90d94c93
guochen2 <524014724@qq.com>
2023-07-14 16:05:18 +0800

    不自动注册时 不进行注册任务

--- src/DotXxlJob.Core/Hosted/JobExecuteHostedService.cs
+++ src/DotXxlJob.Core/Hosted/JobExecuteHostedService.cs
@@ -1,25 +1,33 @@
 using System.Threading;
 using System.Threading.Tasks;
+using DotXxlJob.Core.Config;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace DotXxlJob.Core
 {
     /// <summary>
     ///  NOTE: 负责启动Executor服务，和进行服务注册的宿主服务
     /// </summary>
-    public class JobsExecuteHostedService:BackgroundService 
+    public class JobsExecuteHostedService : BackgroundService
     {
         private readonly IExecutorRegistry _registry;
+        private readonly IOptions<XxlJobExecutorOptions> _options;
 
-        public JobsExecuteHostedService(IExecutorRegistry registry)
+        public JobsExecuteHostedService(IExecutorRegistry registry, IOptions<XxlJobExecutorOptions> options)
         {
             this._registry = registry;
+            _options = options;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            return this._registry.RegistryAsync(stoppingToken);
+            if (_options.Value.AutoRegistry)
+            {
+                return this._registry.RegistryAsync(stoppingToken);
+            }
+            return Task.CompletedTask;
         }
-      
+
     }
 }
